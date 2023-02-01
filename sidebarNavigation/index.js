7382be581ba204b71960da2ce13347af1dcb90c0
VolhaVaitulevich <o.voytulevich@gmail.com>
2023-02-01 23:12:22 +0100

    Update add/delete elements functionality

--- sidebarNavigation/index.js
+++ sidebarNavigation/index.js
@@ -1,18 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
   const documentBody = document.querySelector("body");
-  //create <nav>
+  //create navigation sidebar
   const navigation = document.createElement("nav");
   navigation.className = "navigation";
+
   //create nested links
   const instagramLink = document.createElement("a");
-  instagramLink.className = "navigation__instagram";
+  instagramLink.className = "navigation__network navigation__instagram";
   const facebookLink = document.createElement("a");
-  facebookLink.className = "navigation__facebook";
+  facebookLink.className = "navigation__network navigation__facebook";
   const twitterLink = document.createElement("a");
-  twitterLink.className = "navigation__twitter";
+  twitterLink.className = "navigation__network navigation__twitter";
   const linkedinLink = document.createElement("a");
-  linkedinLink.className = "navigation__linkedin";
-  //create images for links
+  linkedinLink.className = "navigation__network navigation__linkedin";
+
+  //add images for links
   const instagramIcon = document.createElement("i");
   instagramIcon.className = "fa-brands fa-instagram";
 
@@ -25,54 +27,44 @@ document.addEventListener("DOMContentLoaded", function () {
   const linkedinIcon = document.createElement("i");
   linkedinIcon.className = "fa-brands fa-linkedin";
 
-  //add images into links
-  instagramLink.appendChild(instagramIcon);
-  facebookLink.appendChild(facebookIcon);
-  twitterLink.appendChild(twitterIcon);
-  linkedinLink.appendChild(linkedinIcon);
+  // add all elements to navigation sidebar
+  instagramLink.append(instagramIcon);
+  facebookLink.append(facebookIcon);
+  twitterLink.append(twitterIcon);
+  linkedinLink.append(linkedinIcon);
 
-  //add links into <nav>
-  navigation.appendChild(instagramLink);
-  navigation.appendChild(facebookLink);
-  navigation.appendChild(twitterLink);
-  navigation.appendChild(linkedinLink);
-  //add <nav> into body
-  documentBody.appendChild(navigation);
+  navigation.append(instagramLink);
+  navigation.append(facebookLink);
+  navigation.append(twitterLink);
+  navigation.append(linkedinLink);
 
-  //add eventlistener for :hover
-  navigation.addEventListener("mouseover", (event) => {
-    //add <span> elemets
+  //add navigation sidebar to the page
+  documentBody.append(navigation);
+
+  //eventlistener for :hover
+  navigation.addEventListener("mouseenter", (event) => {
     const instagramText = document.createElement("span");
-    instagramText.className = "navigation__instagram-text";
+    instagramText.className = "navigation_text navigation__instagram-text";
     instagramText.innerHTML = "Instagram";
     const facebookText = document.createElement("span");
-    facebookText.className = "navigation__facebook-text";
+    facebookText.className = "navigation_text navigation__facebook-text";
     facebookText.innerHTML = "Facebook";
     const twitterText = document.createElement("span");
-    twitterText.className = "navigation__twitter-text";
+    twitterText.className = "navigation_text navigation__twitter-text";
     twitterText.innerHTML = "Twitter";
     const linkedinText = document.createElement("span");
-    linkedinText.className = "navigation__linkedin-text";
+    linkedinText.className = "navigation_text navigation__linkedin-text";
     linkedinText.innerHTML = "Linkedin";
-    instagramLink.appendChild(instagramText);
-    facebookLink.appendChild(facebookText);
-    twitterLink.appendChild(twitterText);
-    linkedinLink.appendChild(linkedinText);
+
+    instagramLink.prepend(instagramText);
+    facebookLink.prepend(facebookText);
+    twitterLink.prepend(twitterText);
+    linkedinLink.prepend(linkedinText);
   });
 
-  //add eventlistener when mouse is out from navigation
-  navigation.addEventListener("mouseout", (event) => {
-    const instagramLink = document.querySelector(".navigation__instagram");
-    const instagramText = document.querySelector(".navigation__instagram-text");
-    instagramLink.removeChild(instagramText);
-    const facebookLink = document.querySelector(".navigation__facebook");
-    const facebookText = document.querySelector(".navigation__facebook-text");
-    facebookLink.removeChild(facebookText);
-    const twitterLink = document.querySelector(".navigation__twitter");
-    const twitterText = document.querySelector(".navigation__twitter-text");
-    twitterLink.removeChild(twitterText);
-    const linkedinLink = document.querySelector(".navigation__linkedin");
-    const linkedinText = document.querySelector(".navigation__linkedin-text");
-    linkedinLink.removeChild(linkedinText);
+  //eventlistener when mouse is out from navigation
+  navigation.addEventListener("mouseleave", (event) => {
+    const navigationText = document.querySelectorAll(".navigation_text");
+    navigationText.forEach((item) => item.remove());
   });
 });
