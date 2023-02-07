7bfa3662a4f08f9f51ad91fb994f993a2f85acb3
VolhaVaitulevich <o.voytulevich@gmail.com>
2023-02-07 20:12:55 +0000

    Update animation

--- conceptPractice/index.js
+++ conceptPractice/index.js
@@ -4,8 +4,14 @@ const buttonHighlight = document.querySelector(".button__highlight");
 const buttonClear = document.querySelector('.button__clear');
 
 buttonHighlight.addEventListener("click", () => {
+  //to discuss - how to use max attr
+  /* 
+  const maxImageNumber = imageNumber.attributes.max.value; */
+  if (imageNumber.value)
+  {
     const imageToHighlight = document.querySelector(`[data-id='${imageNumber.value}']`);
     imageToHighlight.classList.add('animated');
+  }
 });
 
 buttonClear.addEventListener('click', ()=>{
