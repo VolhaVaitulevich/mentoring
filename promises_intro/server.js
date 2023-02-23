28d74119c6ff6e0a67867abf789630c50d8d5b9b
VolhaVaitulevich <o.voytulevich@gmail.com>
2023-02-23 22:02:49 +0100

    Add post and delete methods

--- promises_intro/server.js
+++ promises_intro/server.js
@@ -1,17 +1,56 @@
+const { response } = require('express');
 const express = require('express');
 const path = require('path');
 
 const port = '8000';
 const app = express();
 
-const POSTS = [{}];
+const POSTS = 
+[
+    {
+        id: "1",
+        title: "Olga"
+    },
+    {
+        id: "2",
+        title: "test"
+    }
+  ];
 
 
-app.get('/', (request, response) => {
-    response.sendFile(path.join(__dirname+'/index.html'));
+app.use('/', express.json());
+
+//TBC
+//check if this is needed here or can be changes in html
+app.get('/index.js', (req, res) => {
+    res.sendFile(path.join(__dirname+'/index.js'));
 })
 
-app.use('/', express.Router());
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname+'/index.html'));
+})
+
+app.get('/posts', (req, res) => {
+    res.status(200).json(POSTS);
+    //add errors
+})
+
+app.post('/posts', (req, res) => {
+    const newPost = req.body;
+    POSTS.push(newPost);
+    res.status(201).json(newPost);
+})
+
+app.delete('/posts/:id', (req, res) => {
+    const postToDeleteIndex = POSTS.findIndex((post) => {
+        post.title === req.body.title;
+    });
+    if (postToDeleteIndex) {
+        POSTS.splice(postToDeleteIndex,1);
+        res.status(200).json(POSTS);
+    }
+})
+
 app.listen(port, () => {
     console.log(`Listen on the port ${port}...`);
 })
